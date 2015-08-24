8b6adae832b39bfc5df333e877c944b033355698
Monica Singh <monicas138@gmail.com>
2015-08-24 17:42:23 +0530

    initializing

--- src/SalesTaxes.java
+++ src/SalesTaxes.java
@@ -2,8 +2,14 @@
  * Created by monicas on 24/08/15.
  */
 public class SalesTaxes {
+
+    public class calculateTax{
+        int items;
+        float sales_tax;
+        
+    }
     public static void main(String[] args) {
         //System.out.print("hello");
-        
+
     }
 }
