32b36ea30312f9009041aff546c41b62afbb1631
Monica Singh <monicas138@gmail.com>
2015-08-24 17:56:38 +0530

    adding item class

--- src/SalesTaxes.java
+++ src/SalesTaxes.java
@@ -6,8 +6,33 @@ public class SalesTaxes {
     public class calculateTax{
         int items;
         float sales_tax;
-        
-    }
+
+    };
+
+    public class Item {
+
+        protected float itemPrice;
+        private String name;
+
+        public String getItemDescription() {
+            return name;
+        }
+
+        public void setItemDescription(String itemName){
+            name = itemName;
+        }
+
+        public float getItemPrice() {
+            return itemPrice;
+        }
+
+        public void setItemPrice(float price) {
+            itemPrice = price;
+
+        }
+    };
+
+
     public static void main(String[] args) {
         //System.out.print("hello");
 
